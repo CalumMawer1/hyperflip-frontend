a5e72f409c052147f7f21927843ad28e50252179
CalumMawer1 <calummawer1@gmail.com>
2025-03-14 22:49:39 -0700

    Add X logo in bottom left corner linking to HyperFlipEVM account

--- src/components/CoinFlip.tsx
+++ src/components/CoinFlip.tsx
@@ -1011,6 +1011,25 @@ export function CoinFlip() {
         </h1>
       </div>
 
+      {/* X Logo Link */}
+      <a 
+        href="https://x.com/HyperFlipEVM" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="absolute bottom-6 left-6 z-10 hover:opacity-80 transition-opacity"
+        aria-label="Follow us on X"
+      >
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="32" 
+          height="32" 
+          viewBox="0 0 24 24" 
+          fill="#04e6e0"
+        >
+          <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+        </svg>
+      </a>
+
       {/* Wallet Connect */}
       <div className="absolute top-6 right-6 z-10">
         <ConnectButton />
