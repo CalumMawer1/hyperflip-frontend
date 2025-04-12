f1cfe29c7f8f54d5eb06b40f30fc1c8bd76428d9
Cole Howard <colehowa@usc.edu>
2025-04-11 19:43:58 -0700

    remove references to unused fonts

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,26 +1,5 @@
 import localFont from 'next/font/local';
 
-export const montserrat = localFont({
-  src: '../assets/fonts/Montserrat.ttf',
-  display: 'swap',
-  variable: '--font-montserrat',
-  preload: true,
-});
-
-export const ibmPlex = localFont({
-  src: '../assets/fonts/IBMPlex.ttf',
-  display: 'swap',
-  variable: '--font-ibmplex',
-  preload: true,
-});
-
-export const poppins = localFont({
-  src: '../assets/fonts/Poppins.ttf',
-  display: 'swap',
-  variable: '--font-poppins',
-  preload: true,
-});
-
 export const tektur = localFont({
   src: '../assets/fonts/Tektur.ttf',
   display: 'swap',
